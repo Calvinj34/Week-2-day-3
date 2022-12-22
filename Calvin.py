ff39c65e5961cc82af56a3931f72c3d79236f444
Calvin Johnson <kingcalvin22@yahoo.com>
2022-12-22 15:07:05 -0700

    wednesday homework

---
+++ Calvin.py
@@ -0,0 +1,11 @@
+
+
+def sqroot():
+    w=int (input("what is the width"))
+    l =int (input("what is the length?"))
+    print(f'your total area is {l*w}')  
+    return int(l * w)
+
+sqroot ()
+
+name = "calvinsqft"
---
+++ circum.py
@@ -0,0 +1,15 @@
+def circum ():
+    radius= int(5)
+    rad = int[radius**2];
+    pie = [3.14];
+
+    circumference = int(rad*pie)
+    print(f'the circumference is{circumference}')
+circum()
+
+# #  w=int (input("what is the width"))
+#     l =int (input("what is the length?"))
+#     print(f'your total area is {l*w}')  
+#     return int(l * w)
+
+# sqroot ()
